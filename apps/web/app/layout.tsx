78144adb36301aef118fcdf6b6b31d15eb3a5a3f
greggbutlerac-debug <greggbutlerac@gmail.com>
2026-07-19 15:53:20 -0400

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,7 @@
 import type { Metadata, Viewport } from 'next';
+
+import ShareExchangeButton from '../components/share-exchange-button';
+
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -113,7 +116,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div
+          style={{
+            position: 'fixed',
+            top: '18px',
+            right: '18px',
+            zIndex: 1000,
+          }}
+        >
+          <ShareExchangeButton />
+        </div>
+
+        {children}
+      </body>
     </html>
   );
 }
