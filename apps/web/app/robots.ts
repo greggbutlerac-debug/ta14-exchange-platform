9a9c043155c7d920d22e3833d44d239fca7b9996
greggbutlerac-debug <greggbutlerac@gmail.com>
2026-08-15 18:27:42 -0400

    Add explicit search crawler policy

---
+++ apps/web/app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/admin/', '/api/'],
+    },
+    sitemap: 'https://ta14exchange.com/sitemap.xml',
+    host: 'https://ta14exchange.com',
+  };
+}
