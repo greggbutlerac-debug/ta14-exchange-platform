aaf436d940924e876f114eef1e5e87e035bdfe04
greggbutlerac-debug <greggbutlerac@gmail.com>
2026-08-15 20:34:57 -0400

    Create public governance showcase detail route

---
+++ apps/web/app/governance-showcase/[registryIdentifier]/page.tsx
@@ -0,0 +1,2 @@
+export { dynamic, generateMetadata } from '../../workspace/ai-governance/registry/showcase/[registryIdentifier]/page';
+export { default } from '../../workspace/ai-governance/registry/showcase/[registryIdentifier]/page';
