31b527d3101170444d78640c2b421b407d815939
greggbutlerac-debug <greggbutlerac@gmail.com>
2026-09-10 22:51:10 -0400

    Activate canonical architecture family on front door

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,6 @@
 import FrontDoorPrototype from '../../../prototypes/front-door-institutional-map-2026-09-09/FrontDoorPrototype';
 import RainEnhancer from './front-door-preview/RainEnhancer';
+import ArchitectureFamilyEnhancer from './front-door-preview/ArchitectureFamilyEnhancer';
 import CanonicalChain24 from './front-door-preview/CanonicalChain24';
 import './front-door-preview/multilingual-rain.css';
 
@@ -7,6 +8,7 @@ export default function HomePage() {
   return (
     <>
       <RainEnhancer />
+      <ArchitectureFamilyEnhancer />
       <FrontDoorPrototype />
       <CanonicalChain24 />
     </>
