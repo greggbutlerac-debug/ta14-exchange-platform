a31ce7002b9ee5fbaf9e87b9d083567f85ac49a8
greggbutlerac-debug <greggbutlerac@gmail.com>
2026-09-10 22:06:18 -0400

    Activate complete approved TA-14 front door

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,14 @@
 import FrontDoorPrototype from '../../../prototypes/front-door-institutional-map-2026-09-09/FrontDoorPrototype';
+import RainEnhancer from './front-door-preview/RainEnhancer';
+import CanonicalChain24 from './front-door-preview/CanonicalChain24';
+import './front-door-preview/multilingual-rain.css';
 
 export default function HomePage() {
-  return <FrontDoorPrototype />;
+  return (
+    <>
+      <RainEnhancer />
+      <FrontDoorPrototype />
+      <CanonicalChain24 />
+    </>
+  );
 }
