fdadba796f6e11de8c85324599ead587d27a5795
greggbutlerac-debug <greggbutlerac@gmail.com>
2026-09-15 22:01:30 -0400

    feat: mount sixth institutional engagement door

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 import FrontDoorPrototype from '../../../prototypes/front-door-institutional-map-2026-09-09/FrontDoorPrototype';
 import RainEnhancer from './front-door-preview/RainEnhancer';
 import ArchitectureFamilyEnhancer from './front-door-preview/ArchitectureFamilyEnhancer';
+import InstitutionalEngagementEnhancer from './front-door-preview/InstitutionalEngagementEnhancer';
 import CanonicalChain24 from './front-door-preview/CanonicalChain24';
 import './front-door-preview/multilingual-rain.css';
 
@@ -9,6 +10,7 @@ export default function HomePage() {
     <>
       <RainEnhancer />
       <ArchitectureFamilyEnhancer />
+      <InstitutionalEngagementEnhancer />
       <FrontDoorPrototype />
       <CanonicalChain24 />
     </>
