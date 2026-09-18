ae9b38561beb3a96acd709fa668f313fe6355a48
greggbutlerac-debug <greggbutlerac@gmail.com>
2026-09-18 18:17:11 -0400

    Mount commercial eighth door on Exchange front door

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,7 @@ import FrontDoorPrototype from '../../../prototypes/front-door-institutional-map
 import RainEnhancer from './front-door-preview/RainEnhancer';
 import ArchitectureFamilyEnhancer from './front-door-preview/ArchitectureFamilyEnhancer';
 import InstitutionalEngagementEnhancer from './front-door-preview/InstitutionalEngagementEnhancer';
+import CommercialDoorEnhancer from './front-door-preview/CommercialDoorEnhancer';
 import CanonicalChain24 from './front-door-preview/CanonicalChain24';
 import './front-door-preview/multilingual-rain.css';
 
@@ -11,6 +12,7 @@ export default function HomePage() {
       <RainEnhancer />
       <ArchitectureFamilyEnhancer />
       <InstitutionalEngagementEnhancer />
+      <CommercialDoorEnhancer />
       <FrontDoorPrototype />
       <CanonicalChain24 />
     </>
