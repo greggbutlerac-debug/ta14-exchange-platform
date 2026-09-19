45d9808ad17cd29979613c7052ce4dadc16b4b09
greggbutlerac-debug <greggbutlerac@gmail.com>
2026-09-19 18:11:36 -0400

    Remove client-side front-door DOM mutation enhancers

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,6 @@
 import FrontDoorPrototype from '../../../prototypes/front-door-institutional-map-2026-09-09/FrontDoorPrototype';
 import RainEnhancer from './front-door-preview/RainEnhancer';
 import ArchitectureFamilyEnhancer from './front-door-preview/ArchitectureFamilyEnhancer';
-import InstitutionalEngagementEnhancer from './front-door-preview/InstitutionalEngagementEnhancer';
-import CommercialDoorEnhancer from './front-door-preview/CommercialDoorEnhancer';
 import CanonicalChain24 from './front-door-preview/CanonicalChain24';
 import './front-door-preview/multilingual-rain.css';
 
@@ -11,8 +9,6 @@ export default function HomePage() {
     <>
       <RainEnhancer />
       <ArchitectureFamilyEnhancer />
-      <InstitutionalEngagementEnhancer />
-      <CommercialDoorEnhancer />
       <FrontDoorPrototype />
       <CanonicalChain24 />
     </>
